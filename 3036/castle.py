806ac112a9b84ab0dd1e9927dda034b565c31d94
it69070084 <69070084@kmitl.ac.th>
2026-07-18 22:17:21 +0700

    Update castle.py

--- 3036/castle.py
+++ 3036/castle.py
@@ -1,15 +1,8 @@
 '''castle'''
 room = int(input())
-remain = room
-walls = 0
-row = 0
-odd_room = room % 2
-for i in range(room):
-    remain -= ((i * 2) +1)
+remain, walls, row = room, 0, 0
+while remain > 0:
+    remain -= (row * 2 + 1)
     row += 1
-    if remain <= 0 :
-        walls = i * 2
-        if row % 2 != odd_room :
-            walls -= 1
-        break
+walls = (row - 1) * 2 - (1 if (row % 2) != (room % 2) else 0)
 print(walls)
